5cecffb388d34b9400cc72edc5f6e17b060a49a9
anlaczek <50656409+anlaczek@users.noreply.github.com>
2019-05-16 10:24:09 +0200

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -56,7 +56,7 @@ public class Movement : MonoBehaviour
     void Update()
     {
         if (CanMove)
-            movementDirection = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal")).normalized;
+            movementDirection = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
         else
             movementDirection = Vector3.zero;
     }
